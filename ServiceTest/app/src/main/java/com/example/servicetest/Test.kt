e9e6ad6cdc63313f41cc68c64bb35b97ed71a1e8
燕晓玉 <yxy1585391@126.com>
2021-01-19 15:34:37 +0800

    kotilin的协变

--- ServiceTest/app/src/main/java/com/example/servicetest/Test.kt
+++ ServiceTest/app/src/main/java/com/example/servicetest/Test.kt
@@ -2,9 +2,31 @@ package com.example.servicetest
 
 inline fun <reified T> getGenericType() = T::class.java
 
+
+
+open class Person(val name: String, val age: Int)
+class Student(name: String, age: Int): Person(name,age)
+class Teacher(name: String, age: Int): Person(name,age)
+
+class SimpleData<out T>(private val data: T?) {
+    fun get(): T? {
+        return data
+    }
+}
+
+
 fun main() {
-    val result1 = getGenericType<String>()
-    val result2 = getGenericType<Int>()
-    println("result1 is $result1")
-    println("result2 is $result2")
+//    val result1 = getGenericType<String>()
+//    val result2 = getGenericType<Int>()
+//    println("result1 is $result1")
+//    println("result2 is $result2")
+
+    val student = Student("Tom",19)
+    val data = SimpleData<Student>(student)
+    handleMyData(data)
+    val studentData = data.get()
+}
+
+fun handleMyData(data: SimpleData<Person>) {
+    val peronData = data.get()
 }
