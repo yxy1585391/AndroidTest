e7d47a761615ac6669e4056be241514805abf744
燕晓玉 <yxy1585391@126.com>
2021-01-21 10:54:54 +0800

    pull解析xml数据

--- NetWorkTest/app/src/main/java/com/example/networktest/MainActivity.kt
+++ NetWorkTest/app/src/main/java/com/example/networktest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.networktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.io.StringReader
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.URL
@@ -17,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sendRequestBtn.setOnClickListener {
-            sendRequestHttpURLConnection()
+            sendRequestWithOkHttp()
         }
     }
 
@@ -54,4 +62,83 @@ class MainActivity : AppCompatActivity() {
             responseText.text = response
         }
     }
+
+    private fun sendRequestWithOkHttp1() {
+        thread {
+            try {
+                val client = OkHttpClient()
+                val requestBody = FormBody.Builder()
+                        .add("username","admin")
+                        .add("password","123456")
+                        .build()
+                val request = Request.Builder()
+                        .url("https://www.baidu.com")
+                        .post(requestBody)
+                        .build()
+                val response = client.newCall(request).execute()
+                val responseData = response.body?.string()
+                if (responseData != null) {
+                    showResponse(responseData)
+                }
+            }catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun sendRequestWithOkHttp() {
+        thread {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+//                http://10.0.2.2/get_data.xml
+                        .url("http://10.0.2.2/get_data.xml")
+                        .build()
+                val response = client.newCall(request).execute()
+                val responseData = response.body?.string()
+                if (responseData != null) {
+                    parseXMLWithPull(responseData)
+                }
+            }catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun parseXMLWithPull(xmlData: String) {
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val xmlPullParser = factory.newPullParser()
+            xmlPullParser.setInput(StringReader(xmlData))
+            var eventType = xmlPullParser.eventType
+            var id = ""
+            var name = ""
+            var version = ""
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val nodeName = xmlPullParser.name
+                when(eventType) {
+                    //开始解析某个节点
+                    XmlPullParser.START_TAG -> {
+                        when(nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    //完成解析某个节点
+                    XmlPullParser.END_TAG -> {
+                        if ("app" == nodeName) {
+                            Log.d("MainActivity","id is $id")
+                            Log.d("MainActivity","name is $name")
+                            Log.d("MainActivity","version is $version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        }catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
 }
