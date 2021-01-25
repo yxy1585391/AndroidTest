d1cd52d378f0b2d48463e6ffeef1e485c44e00b0
燕晓玉 <yxy1585391@126.com>
2021-01-25 10:23:35 +0800

    简化SnackBar方法

---
+++ MaterialTest/app/src/main/java/com/example/materialtest/Snackbar.kt
@@ -0,0 +1,26 @@
+package com.example.materialtest
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+fun View.showSnackbar(text: String, actionText: String? = null, duration: Int = Snackbar.LENGTH_SHORT, block: (() -> Unit)? = null) {
+//    Snackbar.make(this,text,duration).show()
+    val snackbar = Snackbar.make(this,text,duration)
+    if (actionText != null && block != null) {
+        snackbar.setAction(actionText) {
+            block()
+        }
+    }
+    snackbar.show()
+}
+
+fun View.showSnackbar(resId: Int, actionResId: String? = null,duration: Int = Snackbar.LENGTH_SHORT, block: (() -> Unit)? = null) {
+//    Snackbar.make(this,resId,duration).show()
+    val snackbar = Snackbar.make(this,resId,duration)
+    if (actionResId != null && block != null) {
+        snackbar.setAction(actionResId) {
+            block()
+        }
+    }
+    snackbar.show()
+}
